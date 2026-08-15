50bfbc22089c08f36e692e9a3aabcb98213bdbff
Bastian Bukatz <bastian.bukatz@uzh.ch>
2026-08-15 20:26:54 +0200

    added missing import

--- src/schweinehund/app.py
+++ src/schweinehund/app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import date, datetime, timedelta
+from importlib import resources
 import calendar
 import matplotlib
 matplotlib.use('Agg')
